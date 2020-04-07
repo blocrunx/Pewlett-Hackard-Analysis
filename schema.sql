fd5b529c26cd977afc4f4ea24b64d9477a4e2eee
blocrunx <61406569+blocrunx@users.noreply.github.com>
2020-04-06 20:47:09 -0400

    corrected titles keys and added data to tables

--- schema.sql
+++ schema.sql
@@ -52,5 +52,5 @@ title VARCHAR NOT NULL,
 from_date DATE NOT NULL,
 to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-PRIMARY KEY (emp_no)
+PRIMARY KEY (emp_no, title, from_date)
 );
