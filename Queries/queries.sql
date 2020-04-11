9d4ad32e48e13e816a2c0826cea8601895762527
blocrunx <61406569+blocrunx@users.noreply.github.com>
2020-04-11 19:32:24 -0400

    adding project images, queries and file housekeeping

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -190,53 +190,53 @@ ORDER BY last_name;
 
 -- CHALLENGE QUERY 1 - inner employee, titels, salaries
 -- returning emp_no, first_name, last_name, title, salary
-SELECT e.emp_no,
-	   e.first_name,
-	   e.last_name,
+SELECT ei.emp_no,
+	   ei.first_name,
+	   ei.last_name,
 	   ti.title,
 	   ti.from_date,
 	   s.salary
-FROM employees AS e
+INTO ret_eligible
+FROM emp_info AS ei
 INNER JOIN titles AS ti
-ON (e.emp_no = ti.emp_no)
-INNER JOIN salaries AS S
-ON (e.emp_no = s.emp_no);
-
--- Challenge query 2 - remove duplicate names:
--- showing full table with duplicates
-SELECT * FROM
-(SELECT *, count(*) 
-OVER (PARTITION BY
-      first_name,
-      last_name
-    ) AS count
-  FROM emp_title_salary) tableWithCount
-  WHERE tableWithCount.count > 1;
-
-  -- Removing duplicate names:
-  ALTER TABLE emp_title_salary ADD id SERIAL;
-DELETE FROM emp_title_salary WHERE emp_title_salary.id NOT IN 
-(SELECT id FROM (SELECT DISTINCT ON (first_name, last_name) * FROM emp_title_salary) AS foo);
+ON (ei.emp_no = ti.emp_no)
+INNER JOIN salaries AS s
+ON (ei.emp_no = s.emp_no);
 
 -- Return and create table with count of department type and date ordered by decending:
-CREATE TABLE emp_dept_count AS
-SELECT * FROM (SELECT *, count(*) 
-OVER (PARTITION BY title) AS count
-FROM emp_title_salary) tableWithCount
-ORDER BY from_date DESC;
+ALTER TABLE ret_eligible ADD id SERIAL;
+SELECT id, emp_no, first_name, last_name, title, from_date, salary 
+INTO clean_ret_eligible
+FROM (SELECT id, emp_no, first_name, last_name, title, from_date, salary,
+ROW_NUMBER() OVER 
+(PARTITION BY (first_name, last_name) ORDER BY from_date DESC) rn
+FROM ret_eligible) tmp WHERE rn = 1;
 
+-- Show the number of employees from each department
+SELECT title, 
+	   count(*)
+INTO title_count
+FROM clean_ret_eligible
+GROUP BY title; 
+
+-- 3 get mentors
 -- CHALLENGE QUERY 3: Return employee number, first and last name, title,  
 -- from and to date, with birthdays in 1965 for current employees
 SELECT e.emp_no,
 	   e.first_name,
 	   e.last_name,
 	   ti.title,
-	   de.from_date,
-	   de.to_date
+	   ti.from_date,
+	   ti.to_date
+INTO mentor_eligible
 FROM employees AS e
 INNER JOIN titles AS ti
 ON (e.emp_no = ti.emp_no)
-INNER JOIN dept_emp AS de
-ON (e.emp_no = de.emp_no)
 WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
-AND (de.to_date = '9999-01-01')
+AND (ti.to_date = '9999-01-01');
+
+
+-- Show the count of employees eligible for mentorship program and save the table
+SELECT count(*) as eligible_employees
+INTO mentor_eligible_count
+FROM mentor_eligible;
