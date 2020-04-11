3a5730dc7ec99e255ab4c72106baba7e00adab93
blocrunx <61406569+blocrunx@users.noreply.github.com>
2020-04-11 19:59:30 -0400

    challenge.sql

---
+++ challenge.sql
@@ -0,0 +1,52 @@
+-- CHALLENGE QUERY 1 - inner employee, titels, salaries
+-- returning emp_no, first_name, last_name, title, salary
+SELECT ei.emp_no,
+	   ei.first_name,
+	   ei.last_name,
+	   ti.title,
+	   ti.from_date,
+	   s.salary
+INTO ret_eligible
+FROM emp_info AS ei
+INNER JOIN titles AS ti
+ON (ei.emp_no = ti.emp_no)
+INNER JOIN salaries AS s
+ON (ei.emp_no = s.emp_no);
+
+-- Return and create table with count of department type and date ordered by decending:
+ALTER TABLE ret_eligible ADD id SERIAL;
+SELECT id, emp_no, first_name, last_name, title, from_date, salary 
+INTO clean_ret_eligible
+FROM (SELECT id, emp_no, first_name, last_name, title, from_date, salary,
+ROW_NUMBER() OVER 
+(PARTITION BY (first_name, last_name) ORDER BY from_date DESC) rn
+FROM ret_eligible) tmp WHERE rn = 1;
+
+-- Show the number of employees from each department
+SELECT title, 
+	   count(*)
+INTO title_count
+FROM clean_ret_eligible
+GROUP BY title; 
+
+-- 3 get mentors
+-- CHALLENGE QUERY 3: Return employee number, first and last name, title,  
+-- from and to date, with birthdays in 1965 for current employees
+SELECT e.emp_no,
+	   e.first_name,
+	   e.last_name,
+	   ti.title,
+	   ti.from_date,
+	   ti.to_date
+INTO mentor_eligible
+FROM employees AS e
+INNER JOIN titles AS ti
+ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (ti.to_date = '9999-01-01');
+
+
+-- Show the count of employees eligible for mentorship program and save the table
+SELECT count(*) as eligible_employees
+INTO mentor_eligible_count
+FROM mentor_eligible;
